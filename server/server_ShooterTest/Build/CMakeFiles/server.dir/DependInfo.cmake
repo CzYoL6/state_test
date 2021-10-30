0a305b5e2d51aa886e58f7f74d956807c0f81940
CzYoL6 <52570641+CzYoL6@users.noreply.github.com>
2021-10-30 10:49:18 +0800

    用cmake重新构建了项目
    TODO: 将box2d protobuf以静态库的形式添加

---
+++ server/server_ShooterTest/Build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/ikcp.c" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/ikcp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/Player.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/Player.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/PlayerMovement.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/PlayerMovement.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/game.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/game.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/kcpserver.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/kcpserver.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/kcpsession.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/kcpsession.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/message.pb.cc" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/message.pb.cc.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/packet.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/packet.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/random_engine.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/random_engine.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/server_handle.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/server_handle.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/server_main.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/server_main.cpp.o"
+  "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Source/server_send.cpp" "/mnt/e/GameDes1gn/state_test/server/server_ShooterTest/Build/CMakeFiles/server.dir/Source/server_send.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/server_ShooterTest/Build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/Source/Player.cpp.o"
+  "CMakeFiles/server.dir/Source/PlayerMovement.cpp.o"
+  "CMakeFiles/server.dir/Source/game.cpp.o"
+  "CMakeFiles/server.dir/Source/ikcp.c.o"
+  "CMakeFiles/server.dir/Source/kcpserver.cpp.o"
+  "CMakeFiles/server.dir/Source/kcpsession.cpp.o"
+  "CMakeFiles/server.dir/Source/message.pb.cc.o"
+  "CMakeFiles/server.dir/Source/packet.cpp.o"
+  "CMakeFiles/server.dir/Source/random_engine.cpp.o"
+  "CMakeFiles/server.dir/Source/server_handle.cpp.o"
+  "CMakeFiles/server.dir/Source/server_main.cpp.o"
+  "CMakeFiles/server.dir/Source/server_send.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
